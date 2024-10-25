ef2099be8ce2ae1d7a137fa9dd6779361f1837a9
Masaad Khan <masaadkhan123@gmail.com>
2024-10-25 00:08:17 +0000

    First commit adding my env

---
+++ script.sh
@@ -0,0 +1,4 @@
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
